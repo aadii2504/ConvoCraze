9ffc5fc0228a5988f363eaae8d4dbc600ebb8500
Aadii2504 <adityadhaygude25@gmail.com>
2024-06-19 23:22:02 +0530

    Created the Conversations Section
    update the logic of conversation  section

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations';
 
 
 const Conversations = () => {
-  // useGetConversations();
+   const {loading ,  conversations } = useGetConversations(); 
+   console.log("CONVERSATIONS : " , conversations);
   return (
     <div className='py-2 flex flex-col overflow-auto '>
         <Conversation />
--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -1,7 +1,38 @@
-// import React from 'react'
 
-// const useGetConversations = () => {
-//     const [loading, setLoading] = useState(second)
-// }
+import React, { useState ,  useEffect } from 'react'
+import toast from 'react-hot-toast'
 
-// export default useGetConversations;
+
+const useGetConversations = () => {
+    const [loading, setLoading] = useState()
+    const [conversations, setConversations] = useState([])
+
+    useEffect(() => {
+        const getConversations = async () => {
+            setLoading(true);
+            try {
+                const res  = await fetch('/api/users');
+                const data =  await res.json();
+                if(data.error) {
+                    throw new Error(data.error);
+                }
+                setConversations(data)
+            } catch (error) {
+                toast.error(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+
+
+        getConversations();
+    }, []);
+
+
+    return {loading , conversations };  
+
+}
+
+export default useGetConversations;
+ 
