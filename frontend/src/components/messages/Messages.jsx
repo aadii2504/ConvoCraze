c3d6ef0e12aa66566b0cf94ea5014e2d5646eb53
Aadii2504 <adityadhaygude25@gmail.com>
2024-06-27 21:48:20 +0530

    updated mesaages

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
 
 
 const Messages = () => {
+  const {messages , loading}  =  useGetMessages()
+  console.log("messages:" , messages);
   return (
     <div className='px-4 flex-1 overflow-auto'>
         <Message/>
