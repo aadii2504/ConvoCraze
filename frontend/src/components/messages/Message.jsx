b7239935a624350a85ae469e18c9086aaea5ab18
Aadii2504 <adityadhaygude25@gmail.com>
2024-06-28 22:09:09 +0530

    added new  real  time chat feature

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContex';
+import useConversations from '../../zustand/useConversations';
+import { extractTime } from '../../utils/extractTime';
 
-const Message = () => {
+const Message = ({message}) => {
+  const {authUser} = useAuthContext();
+  const {selectedConversation } = useConversations();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt); 
+  const chatClassName =  fromMe ? 'chat-end' : 'chat-start';
+  const profilePic =  fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor =  fromMe ? 'bg-green-500' : "";
+
+  
+  
   return (
-    <div className='chat chat-end'>
+    <div className= {`chat ${chatClassName}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img src="https://res.cloudinary.com/https-vanessamachi-github-io-portfolio-reactjs/image/upload/v1580918618/avatar-LK-03_jvxpxb.png" alt="Tailwind CSS chat bubble component" />
+                <img src= {profilePic} />
             </div>
         </div>
-        <div className={`chat-bubble text-white bg-blue-500`}> Hi!  What is up?</div>
-        <div className={`chat-footer opacity-50  text-xs flex gap-1 items-center`}>12:42</div>
+        <div className={`chat-bubble text-white  ${bubbleBgColor}`}> {message.message}</div>
+        <div className={`chat-footer opacity-50  text-xs flex gap-1 items-center`}>{formattedTime}</div>
     </div>
   )
 }
