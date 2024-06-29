0570bfb6aae7d4e0b0d022644e1f836cbbcb5d91
Aadii2504 <adityadhaygude25@gmail.com>
2024-06-29 19:39:37 +0530

    added notification sound to the  incoming messages   and also supporting  the  real time supports

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react'
 import { useSocketContext } from '../context/SocketContext'
 import useConversations from '../zustand/useConversations';
 
+// import notificationSound from "../assets/sounds/notification.mp3";
+
+import notificationSound from "../assets/sounds/notification.mp3";
+
 const useListenMessages = () => {
     const { socket } = useSocketContext();
     const  { messages, setMessages} = useConversations();
@@ -9,6 +13,8 @@ const useListenMessages = () => {
     useEffect(() => {
     socket?.on("newMessage" , (newMessage) => {
         newMessage.shouldShake = true;
+        const sound  =  new Audio(notificationSound);
+        sound.play();
         setMessages([...messages, newMessage])
     })
 
