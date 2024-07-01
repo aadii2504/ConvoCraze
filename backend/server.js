11e4e72b252d970b57a6007cff6bd937cce783fb
Aadii2504 <adityadhaygude25@gmail.com>
2024-07-01 23:30:00 +0530

    Done now Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express  from "express";
 import dotenv from "dotenv";
 import cookieParser  from "cookie-parser";
@@ -14,6 +15,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT =  process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 
 dotenv.config();
 
@@ -25,11 +28,11 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 
-app.get("/" ,(req, res) => {
-    // root route http://localhost:3000/
-    res.send("Hello World!");
-});
+app.use(express.static(path.join(__dirname , "/frontend/dist")))
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () =>  {
