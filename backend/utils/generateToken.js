116423ed13a6d07a7b737385ed8ab0108074e647
Aadii2504 <adityadhaygude25@gmail.com>
2024-09-09 12:55:27 +0530

    final commit

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie =  (userId , res) => {
     maxAge : 15 * 24  *  60 * 60 *  1000,
     httpOnly : true, /// to prevent xss attack  // to prevent client side scripting 
     sameSie:"strict", // to make sure that the cookie is only accessible by the main domain   CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV === "develiopment",
+    secure: process.env.NODE_ENV !== "development",
   });
 };
 
